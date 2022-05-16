f8f029c28003c56823a6d13f6c2725afd214ee84
Davi Santos <102557963+DaviSantosDEV@users.noreply.github.com>
2022-05-16 14:16:08 -0300

    Add files via upload

---
+++ Exercicio-3.js
@@ -0,0 +1,29 @@
+let e = 1
+while (e<=100){
+    console.log(e)
+    e = e + 1
+}
+
+// ATIVIDADE 2
+
+let d = 50
+while (d<=100){
+    console.log(d)
+    d = d + 1
+}
+
+// ATIVIDADE 3
+
+let b = 10
+while (b>=0){
+    console.log(b)
+    b = b - 1
+}
+
+// ATIVIDADE 4
+
+let a = 0
+while (a<=10){
+    console.log(a*2)
+    a = a + 1
+}
